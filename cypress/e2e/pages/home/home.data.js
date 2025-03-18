f2f74f8980437ebad5bee06a632982615c615dc6
GOBCAN\ext-dgommar <daniel.gomez@tecnicascompetitivas.com>
2025-03-18 10:50:09 +0000

    Creacion archivos HomePage

---
+++ cypress/e2e/pages/home/home.data.js
@@ -0,0 +1,11 @@
+export class HomeData{
+    static get phones(){
+        return {
+            samsungGalaxyS6: 'Samsung galaxy s6',
+            nokiaLumia1520: 'Nokia lumia 1520',
+            nexus6: 'Nexus 6'
+        }
+    }
+
+    static get laptops(){}
+}
