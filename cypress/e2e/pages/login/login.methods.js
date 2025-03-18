c036cd478d8742cd3171ea7873c9b3c324672214
GOBCAN\ext-dgommar <daniel.gomez@tecnicascompetitivas.com>
2025-03-18 18:21:54 +0000

    Caso de prueba 4: Inicio de sesión inválido

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -1,4 +1,5 @@
 import { Logger } from "../../util/logger";
+import { CommonPageMethods } from "../common-page/common-page.methods";
 import { LoginElements } from "./login.elements";
 
 export class LoginMethods{
@@ -24,4 +25,8 @@ export class LoginMethods{
         Logger.subStep('Click on "Login" button');
         this.clickOnLoginButton()
     }
+
+    static verifyWrongPasswordMessage(){
+        CommonPageMethods.verifyAlert('Wrong password.');
+        }
 }
--- cypress/e2e/tests/autentication.cy.js
+++ cypress/e2e/tests/autentication.cy.js
@@ -6,6 +6,7 @@ import { Logger } from "../util/logger";
 
 const existingUser = LoginData.validCredentials.username;
 const existingPassword = LoginData.validCredentials.password;
+const invalidPassword = LoginData.invalidCredentials.password;
 
 describe(CommonPageData.testSuites.autentication, ()=>{
     it('Inicio de sesión válido',()=>{
@@ -29,6 +30,29 @@ describe(CommonPageData.testSuites.autentication, ()=>{
         cy.wait(2000)
         Logger.verification('Verificar que se redirige al usuario a la página de inicio.');
         CommonPageMethods.verifySignedUser(existingUser);
+        
+    })
 
+    it('Inicio de sesión inválido',()=>{
+        Logger.stepNumber(1);
+        Logger.step('Navegar a la página de inicio.');
+        CommonPageMethods.navigateToDemoBlaze();
+
+        Logger.stepNumber(2);
+        Logger.step('Hacer clic en "Log in" en la barra de navegación.');
+        CommonPageMethods.clickOnLoginOption();
+
+        Logger.stepNumber(3);
+        Logger.step('Ingresar un nombre de usuario y/o contraseña inválidos.');
+        LoginMethods.insertUserName(existingUser);
+        LoginMethods.insertPassword(invalidPassword);       
+
+        Logger.stepNumber(4);
+        Logger.step('Hacer clic en "Log in" para iniciar sesión.');
+        LoginMethods.clickOnLoginButton();
+
+        cy.wait(2000)
+        Logger.verification('Verificar que se muestra un mensaje de error indicando que el inicio de sesión ha fallado.');
+        LoginMethods.verifyWrongPasswordMessage();
     })
 })
