ef76a21ca8fbd7480a96ff160f29bd3c7fa22cec
GOBCAN\ext-dgommar <daniel.gomez@tecnicascompetitivas.com>
2025-03-18 11:33:10 +0000

    Creación archivos Place-Order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData{
+    static get testData(){
+        return{
+            name: 'Test name',
+            country: 'United States',
+            city:'Boston',
+            creditCardNumber:'4332 0857 9510 1486',
+            month: 12,
+            year: 2024
+        }
+    }
+}
