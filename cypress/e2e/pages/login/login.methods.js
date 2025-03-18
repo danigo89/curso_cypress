2bcbe82fae4517176996c425d3ca2deaadefa6ea
GOBCAN\ext-dgommar <daniel.gomez@tecnicascompetitivas.com>
2025-03-18 10:08:23 +0000

    Fix report

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -2,11 +2,11 @@ import { LoginElements } from "./login.elements";
 
 export class LoginMethods{
     static insertUserName(username){
-        LoginElements.textboxes.username.type(username); // type() usado para insertar
+        LoginElements.textboxes.username.invoke('val', username); // type() usado para insertar
     }
 
     static insertPassword(password){
-        LoginElements.textboxes.password.type(password);
+        LoginElements.textboxes.password.invoke('val', password);
     }
 
     static clickOnLoginButton(){
